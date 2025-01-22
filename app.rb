99d8ced774b89830580c765ec84b269b9fc98ce2
AbdulCab <151527636+AbdulCab@users.noreply.github.com>
2025-01-22 20:20:04 +0100

    hoho
    Bilder + db

--- app.rb
+++ app.rb
@@ -1,18 +1,21 @@
 require 'sinatra'
-require 'sinatra\reloader'
+require 'sinatra/reloader'
 require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+db = SQLite3::Database.new "db/pokemon.db"
+db.results_as_hash = true 
+
 get('/') do
-  slim("index")
+
+  slim(:index)
 end
 
 get('/pokemons') do
-  db = SQLite3::Database.new("db/pokemon.db")
-  db.results_as_hash = true
-  result = db.execute("SELECT * FROM Pokemons")
-  p result
-  
+
+  @pokemons = db.execute("SELECT p.id, p.name, t1.name AS type1, t2.name AS type2, p.hp, p.attack, p.defense, p.sp_attack, p.sp_defense, p.speed FROM Pokemons p LEFT JOIN types t1 ON p.type1 = t1.id LEFT JOIN types t2 ON p.type2 = t2.id")
+  p @pokemons
+  slim(:pokemon)
 end
 
